6d1c4372f68fabf4e198bed05e8869163c6adf5f
Mike <23509561+mik-0@users.noreply.github.com>
2022-06-06 19:49:13 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : process.env.DEPLOY_URL;
+export const server = dev ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_DEPLOY_URL;
